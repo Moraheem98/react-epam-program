9712953e1238a6038c74fd6e0b5fa936d46ab94a
moraheem98 <mojoraheem@icloud.com>
2022-09-09 11:00:08 +0100

    cypress tests for search queries

--- task-2/cypress/e2e/1-getting-started/todo.cy.js
+++ task-2/cypress/e2e/1-getting-started/todo.cy.js
@@ -15,12 +15,3 @@ describe('Searching for movie', () => {
 		cy.contains('Fifty Shades Freed');
 	});
 });
-
-describe('Searching for movie', () => {
-	it('verify search results match search query', () => {
-		cy.visit('http://localhost:3000/search'); ///change url
-		cy.get('input').type('Fifty Shades Freed');
-		cy.contains('submit').click();
-		cy.contains('Fifty Shades Freed'); // look for id
-	});
-});
